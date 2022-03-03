50e7621183e55c30d1a55f5a6329725e989135c6
Junki Sato <j9sato11@gmail.com>
2022-03-03 10:47:28 -0700

    finished phase2 Added Error Handler"

--- backend/app.js
+++ backend/app.js
@@ -1,20 +1,21 @@
-// Importing Packages ------------------------------
+// ------------ Importing Packages ---------------------
 const express = require("express");
-const morgan = require("morgan");
+const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const csurf = require("csurf");
 const helmet = require("helmet");
-const cookieParser = require("cookie-parser");
+const morgan = require("morgan");
+const { ValidationError } = require("sequelize");
 
-// Importing files ------------------------------
+// ------------ Importing files ---------------------
 const { environment } = require("./config");
 const routes = require("./routes");
 
-// Setup/Initializing --------------------------------
+// ------------ Setup/Initializing -----------------------
 const isProduction = environment === "production";
 const app = express();
 
-// Middleware ------------------------
+// ------------ Middleware ---------------
 app.use(morgan("dev"));
 app.use(cookieParser());
 app.use(express.json());
@@ -43,9 +44,41 @@ app.use(
   })
 );
 
-// Routes ------------------------
-
+// ------------ Routes ---------------
 app.use(routes);
 
-// Export app --------------------
+// ------------ ERROR HANDLERS ---------------
+
+// Catch unhandled requests and forward to error handler.
+app.use((_req, _res, next) => {
+  const err = new Error("The requested resource couldn't be found.");
+  err.title = "Resource Not Found";
+  err.errors = ["The requested resource couldn't be found."];
+  err.status = 404;
+  next(err);
+});
+
+// Process sequelize errors
+app.use((err, _req, _res, next) => {
+  // check if error is a Sequelize error:
+  if (err instanceof ValidationError) {
+    err.errors = err.errors.map(e => e.message);
+    err.title = "Validation error";
+  }
+  next(err);
+});
+
+// Error formatter
+app.use((err, _req, res, _next) => {
+  res.status(err.status || 500);
+  console.error(err);
+  res.json({
+    title: err.title || "Server Error",
+    message: err.message,
+    errors: err.errors,
+    stack: isProduction ? null : err.stack,
+  });
+});
+
+// ------------ Export app -----------
 module.exports = app;
