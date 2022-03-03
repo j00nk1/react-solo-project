966b676d8fc5f1306c31f11e8d2218f052903742
Junki Sato <j9sato11@gmail.com>
2022-03-03 12:19:30 -0700

    created login route

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,33 +1,42 @@
 const router = require("express").Router();
-const asyncHandler = require("express-async-handler");
 
-const { setTokenCookie } = require("../../utils/auth.js");
-const { User } = require("../../db/models");
+const sessionRouter = require("./session");
+const usersRouter = require("./users");
+
+router.use("/session", sessionRouter);
+
+router.use("/users", usersRouter);
+
+router.post("/test", (req, res) => {
+  res.json({ requestBody: req.body });
+});
+
 // ↓=============TESTING ROUTES=================↓
-router.get(
-  "/set-token-cookie",
-  asyncHandler(async (_req, res) => {
-    const user = await User.findOne({
-      where: {
-        username: "FakeUser1",
-      },
-    });
-    setTokenCookie(res, user);
-    return res.json({ user });
-  })
-);
 
-// GET /api/restore-user
-const { restoreUser } = require("../../utils/auth.js");
-router.get("/restore-user", restoreUser, (req, res) => {
-  return res.json(req.user);
-});
+// router.get(
+//   "/set-token-cookie",
+//   asyncHandler(async (_req, res) => {
+//     const user = await User.findOne({
+//       where: {
+//         username: "FakeUser1",
+//       },
+//     });
+//     setTokenCookie(res, user);
+//     return res.json({ user });
+//   })
+// );
 
-// GET /api/require-auth
-const { requireAuth } = require("../../utils/auth.js");
-router.get("/require-auth", requireAuth, (req, res) => {
-  return res.json(req.user);
-});
+// // GET /api/restore-user
+// const { restoreUser } = require("../../utils/auth.js");
+// router.get("/restore-user", restoreUser, (req, res) => {
+//   return res.json(req.user);
+// });
+
+// // GET /api/require-auth
+// const { requireAuth } = require("../../utils/auth.js");
+// router.get("/require-auth", requireAuth, (req, res) => {
+//   return res.json(req.user);
+// });
 
 // ↑=============TESTING ROUTES=================↑
 
