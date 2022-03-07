e5f79eafd508596429fa65256b786f11d12abaac
Junki Sato <j9sato11@gmail.com>
2022-03-06 22:08:09 -0700

    small change on the h1 in the landing page

--- frontend/src/components/Landing/index.js
+++ frontend/src/components/Landing/index.js
@@ -5,8 +5,14 @@ import yen from "../Navigation/images/yen-logo.png";
 function Landing() {
   return (
     <>
-      <h1>Welcome to</h1>
-      <img src={yen} alt="yen" style={{ display: "block", margin: "0 auto" }} />
+      <h1 style={{ fontFamily: "serif" }}>
+        Welcome to
+        <img
+          src={yen}
+          alt="yen"
+          style={{ display: "block", margin: "0 auto" }}
+        />
+      </h1>
     </>
   );
 }
